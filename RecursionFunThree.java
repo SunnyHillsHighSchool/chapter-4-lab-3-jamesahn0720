932d9fc43a17c6210e69f0283c94f24add9d96d8
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 23:30:17 -0700

    Update RecursionFunThree.java

--- RecursionFunThree.java
+++ RecursionFunThree.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name - 
 //Date -
 //Class -
@@ -8,18 +8,18 @@ import static java.lang.System.*;
 
 public class RecursionFunThree
 {
-	/* luckyThrees will return the count of 3s in the number
-	 * unless the 3 is at the front and then it does not count
-	 * 3 would return 0
-	 * 31332  would return 2
-	 * 134523 would return 2
-	 * 3113  would return 1
-	 * 13331 would return 3
-	 * 777337777  would return 2
-	 * the solution to this problem must use recursion
-	 */
 	public static int luckyThrees( long number )
 	{
-		return 0;
+		int count = 0;
+        while (number > 0) {
+            if (number % 100 == 77) {
+                number /= 10;
+                count += 14;
+            } else if (number % 10 == 7) {
+                count += 1;
+            }
+            number /= 10;
+        }
+        return count;
 	}
 }
