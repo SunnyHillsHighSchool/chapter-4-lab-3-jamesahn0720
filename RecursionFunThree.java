d827394c0aeec392301e561de75b8d9b0e9fe146
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 20:50:48 -0700

    Update RecursionFunThree.java

--- RecursionFunThree.java
+++ RecursionFunThree.java
@@ -10,16 +10,14 @@ public class RecursionFunThree
 {
 	public static int luckyThrees( long number )
 	{
-		int count = 0;
-        while (number > 0) {
-            if (number % 100 == 77) {
-                number /= 10;
-                count += 14;
-            } else if (number % 10 == 7) {
-                count += 1;
-            }
-            number /= 10;
-        }
-        return count;
+	if(number<10)
+    {
+      return 0;
+    }
+    if(number%10==3)
+    {
+      return 1+luckyThrees(number/10);
+    }
+		return 0+luckyThrees(number/10);
 	}
 }
